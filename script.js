1cc2429f7a862d1b04b15c5d3d06b42b237c94ac
dolapo <husseindollarp@yahoo.com>
2024-04-25 01:09:46 -0700

    Understaning functions calling other functions

--- script.js
+++ script.js
@@ -60,3 +60,33 @@ const newPassport = function (person) {
 
 newPassport(qudus);
 checkIn(flight, qudus);
+
+// Functions accepting callback functions
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
+
+['jonas', 'Martha', 'Adam'].forEach(high5);
